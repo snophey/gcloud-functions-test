15143d4ea6fbe8c83c83ef5712a4db72e19c9b32
quarkusio <no-reply@quarkus.io>
2023-07-16 12:03:49 +0000

    Initial commit

---
+++ src/main/java/dev/arov/googlecloudfunctions/GreetingFunqy.java
@@ -0,0 +1,17 @@
+package dev.arov.googlecloudfunctionshttp;
+
+import io.quarkus.funqy.Funq;
+import io.smallrye.mutiny.Uni;
+
+public class GreetingFunqy {
+
+    @Funq
+    public String funqy() {
+        return "Make it funqy";
+    }
+
+    @Funq
+    public Uni<String> funqyAsync() {
+        return Uni.createFrom().item(() -> "Make it funqy asynchronously");
+    }
+}
